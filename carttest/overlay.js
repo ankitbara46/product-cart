9d80b97234e3ae8220d8b512dd4b8aa90d0f4dc7
Ankit Jain <ankjain9@publicisgroupe.net>
2018-10-31 16:38:49 +0530

    first commit

---
+++ carttest/overlay.js
@@ -0,0 +1,16 @@
+var overlay = (function(){
+	
+	return function(alertData){
+		
+		return {
+				show: function(){
+					//alert(alertData);
+				},
+				
+				hide: function(){
+					//alert(alertData+'123');
+				}
+		}
+			
+	}	
+})();
